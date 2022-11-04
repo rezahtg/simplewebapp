43d4ac572622e54262be2bba3d5dfc06c3420ecd
rezahtg <reza.hutagaol@students.amikom.ac.id>
2022-11-04 15:12:30 +0700

    "Adding thymeleaf and show list book"

--- src/main/java/com/example/simplewebapp/model/Book.java
+++ src/main/java/com/example/simplewebapp/model/Book.java
@@ -91,8 +91,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", publisher=" + publisher +
-                ", authors=" + authors +
                 '}';
     }
 }
